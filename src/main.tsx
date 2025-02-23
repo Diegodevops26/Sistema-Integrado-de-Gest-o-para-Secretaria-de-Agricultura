88ef8bd3398b8a0f7b742ae3e583caa2f96298a2
Diego Santos <63413446+Diegodevops26@users.noreply.github.com>
2025-02-23 12:37:07 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import App from "./App";
+import Routes from "./routes/appRoutes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <Routes />
         </Provider>
     </React.StrictMode>
 );
